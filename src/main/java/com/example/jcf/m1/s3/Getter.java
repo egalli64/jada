fa2ec16c594252ecde02d66c0b11035c8aba5657
Emanuele <egalli64@gmail.com>
2023-09-30 11:56:52 +0200

    inner class

---
+++ src/main/java/com/example/jcf/m1/s3/Getter.java
@@ -0,0 +1,18 @@
+/*
+ * Introduction to Java Collections Framework
+ * 
+ * https://github.com/egalli64/jcf
+ */
+package com.example.jcf.m1.s3;
+
+/**
+ * An interface
+ */
+interface Getter {
+    /**
+     * A method
+     * 
+     * @return a value
+     */
+    int getValue();
+}
