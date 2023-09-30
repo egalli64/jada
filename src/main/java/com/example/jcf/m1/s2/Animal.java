d6a740feffb4cf73d254a118c8b6110cef51fab6
Emanuele <egalli64@gmail.com>
2023-09-30 11:42:39 +0200

    generic

---
+++ src/main/java/com/example/jcf/m1/s2/Animal.java
@@ -0,0 +1,12 @@
+/*
+ * Introduction to Java Collections Framework
+ * 
+ * https://github.com/egalli64/jcf
+ */
+package com.example.jcf.m1.s2;
+
+/**
+ * An interface
+ */
+public interface Animal {
+}
---
+++ src/main/java/com/example/jcf/m1/s2/Dog.java
@@ -0,0 +1,12 @@
+/*
+ * Introduction to Java Collections Framework
+ * 
+ * https://github.com/egalli64/jcf
+ */
+package com.example.jcf.m1.s2;
+
+/**
+ * A class
+ */
+public class Dog implements Animal {
+}
