41854ecf25958b152b6e1aa6d62b77bcec8f23e3
Emanuele <egalli64@gmail.com>
2023-10-02 09:29:13 +0200

    linked list

---
+++ src/main/java/com/example/jcf/m2/s05/exercise/MyIntList.java
@@ -0,0 +1,46 @@
+/*
+ * Introduction to Java Collections Framework
+ * 
+ * https://github.com/egalli64/jcf
+ */
+package com.example.jcf.m2.s05.exercise;
+
+/**
+ * Exercise, create a Linked List for primitive integer.
+ */
+public class MyIntList {
+    private Node head;
+    private Node tail;
+
+    /**
+     * No-arg constructor. The list should be empty.
+     */
+    public MyIntList() {
+        // TODO
+    }
+
+    /**
+     * Add a new element (where?)
+     * 
+     * @param value the value for the new element
+     */
+    public void add(int value) {
+        // TODO
+    }
+
+    /**
+     * Remove an element (form which position?)
+     * 
+     * @return the value of the removed element
+     * @throws MyIntListException in case of problem
+     */
+    public int remove() throws MyIntListException {
+        return Integer.MIN_VALUE;
+    }
+
+    @Override
+    public String toString() {
+        // TODO: should print all the elements in the list
+        return String.valueOf(head) + " ... " + String.valueOf(tail);
+    }
+}
