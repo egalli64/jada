8ba468a59a6f0986ebf0efe194fce003d475edd8
Emanuele <egalli64@gmail.com>
2023-10-02 09:11:16 +0200

    An arrayList as a sequence

--- src/main/java/com/example/jcf/m2/s04/ArrayListSequence.java
+++ src/main/java/com/example/jcf/m2/s04/ArrayListSequence.java
@@ -6,33 +6,36 @@
 package com.example.jcf.m2.s04;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.SequencedCollection;
 
 /**
  * ArrayList, using SequencedCollection methods
  */
 public class ArrayListSequence {
     /**
-     * A few methods from ArrayList
+     * Using an ArrayList as a SequencedCollection
      * 
      * @param args not used
      */
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        System.out.println("An empty list: " + list);
+        SequencedCollection<Integer> seq = new ArrayList<>();
+        System.out.println("An empty sequence: " + seq);
 
         // adding an element in the last position
-        list.addLast(56);
+        seq.addLast(56);
         // adding an element in the first position
-        list.addFirst(18);
-        System.out.println("After adding elements: " + list);
+        seq.addFirst(18);
+        System.out.println("After adding elements: " + seq);
 
-        System.out.println("First element is: " + list.getFirst());
-        System.out.println("Last element is: " + list.getLast());
+        seq = seq.reversed();
+        System.out.println("After reversing: " + seq);
 
-        System.out.println("First element was: " + list.removeFirst());
-        System.out.println("Last element was: " + list.removeLast());
+        System.out.println("First element is: " + seq.getFirst());
+        System.out.println("Last element is: " + seq.getLast());
 
-        System.out.println("Now list is: " + list);
+        System.out.println("First element was: " + seq.removeFirst());
+        System.out.println("Last element was: " + seq.removeLast());
+
+        System.out.println("Now the sequence is: " + seq);
     }
 }
