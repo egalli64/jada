bed5aed9d6434d696f3772e1fd4a2dbeb5b2b9f0
Emanuele <egalli64@gmail.com>
2023-10-02 10:31:52 +0200

    array deque

---
+++ src/main/java/com/example/jcf/m2/s06/ArrayDequeExample.java
@@ -0,0 +1,32 @@
+/*
+ * Introduction to Java Collections Framework
+ * 
+ * https://github.com/egalli64/jcf
+ */
+package com.example.jcf.m2.s06;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * ArrayDeque
+ */
+public class ArrayDequeExample {
+    /**
+     * Using push() and pop() on ArrayDeque
+     * 
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        Deque<Integer> deque = new ArrayDeque<>(List.of(42, -27, 99, 12));
+        System.out.println(deque);
+
+        deque.push(18);
+        System.out.println("Stack: push adds new first (left) element " + deque);
+
+        Integer popped = deque.pop();
+        System.out.println("Stack: pop removes the first (left) element " + popped);
+        System.out.println(deque);
+    }
+}
---
+++ src/main/java/com/example/jcf/m2/s06/exercise/MyIntListArray.java
@@ -0,0 +1,43 @@
+/*
+ * Introduction to Java Collections Framework
+ * 
+ * https://github.com/egalli64/jcf
+ */
+package com.example.jcf.m2.s06.exercise;
+
+/**
+ * TODO: A statically sized array based list of integers
+ * 
+ * @see MyIntList
+ */
+public class MyIntListArray implements MyIntList {
+    // TODO: private properties
+
+    public MyIntListArray() {
+        // TODO
+    }
+
+    @Override
+    public void add(int value) {
+        // TODO
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int get(int pos) {
+        // TODO
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public String toString() {
+        // TODO
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
