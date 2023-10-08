00cb98f31d0ce0ff1fcac93e0417d730d96cb96a
Emanuele <egalli64@gmail.com>
2023-10-08 20:40:29 +0200

    fix header

--- src/main/java/com/example/jada/m1/s02/Dog.java
+++ src/main/java/com/example/jada/m1/s02/Dog.java
@@ -1,7 +1,7 @@
 /*
- * Introduction to Java Collections Framework
+ * Introduction to Data Structures in Java
  * 
- * https://github.com/egalli64/jcf
+ * https://github.com/egalli64/jada
  */
 package com.example.jada.m1.s02;
 
