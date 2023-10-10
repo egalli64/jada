afef8d2c259a63822205c96565adc0e801baf675
Emanuele <egalli64@gmail.com>
2023-10-10 21:34:22 +0200

    a simple record

---
+++ src/main/java/com/example/jada/m1/s08/Simple.java
@@ -0,0 +1,16 @@
+/*
+ * Introduction to Data Structures in Java
+ * 
+ * https://github.com/egalli64/jada
+ */
+package com.example.jada.m1.s08;
+
+/**
+ * A simple record
+ */
+record Simple(String s) {
+}
+
+// this won't compile, records are final!
+//record Wrong(int value) extends Simple {
+//}
---
+++ src/main/java/com/example/jada/m1/s08/SimpleMain.java
@@ -0,0 +1,34 @@
+/*
+ * Introduction to Data Structures in Java
+ * 
+ * https://github.com/egalli64/jada
+ */
+package com.example.jada.m1.s08;
+
+/**
+ * Using a simple record
+ */
+public class SimpleMain {
+    public static void main(String[] args) {
+        Simple simple = new Simple("hello");
+
+        System.out.println("Simple record to string: " + simple);
+        System.out.println("Accessing a record field: " + simple.s());
+        System.out.println("Simple record hash code: " + simple.hashCode());
+
+        Simple alias = simple;
+        System.out.println("Two references to the same record are equal: " + simple.equals(alias));
+
+        Simple same = new Simple("hello");
+        System.out.println("Two different records are equal if their properies are equal: " + simple.equals(same));
+
+        Simple other = new Simple("goodbye");
+        System.out.println("Comparing two different records: " + simple.equals(other));
+
+        System.out.println("Comparing a record against null: " + simple.equals(null));
+
+        Simple nullRec = new Simple(null);
+        System.out.println("A record with null in it: " + nullRec);
+        System.out.println("Comparing to a record with null in it: " + nullRec.equals(null));
+    }
+}
