3ecf6f04ac35c68a21e2a9856e5f5d71dd1c7934
Emanuele <egalli64@gmail.com>
2023-10-11 08:36:03 +0200

    A simple java bean

---
+++ src/main/java/com/example/jada/m1/s07/SimpleMain.java
@@ -0,0 +1,34 @@
+/*
+ * Introduction to Data Structures in Java
+ * 
+ * https://github.com/egalli64/jada
+ */
+package com.example.jada.m1.s07;
+
+/**
+ * Using a simple record
+ */
+public class SimpleMain {
+    public static void main(String[] args) {
+        Simple simple = new Simple("hello");
+
+        System.out.println("Simple bean to string: " + simple);
+        System.out.println("Accessing a bean field: " + simple.getS());
+        System.out.println("Simple bean hash code: " + simple.hashCode());
+
+        Simple alias = simple;
+        System.out.println("Two references to the same bean are equal: " + simple.equals(alias));
+
+        Simple same = new Simple("hello");
+        System.out.println("Two different beans are equal if their fields are equal: " + simple.equals(same));
+
+        Simple other = new Simple("goodbye");
+        System.out.println("Comparing two different beans: " + simple.equals(other));
+
+        System.out.println("Comparing a bean against null: " + simple.equals(null));
+
+        Simple nullBean = new Simple(null);
+        System.out.println("A bean with null in it: " + nullBean);
+        System.out.println("Comparing to a bean with null in it: " + nullBean.equals(null));
+    }
+}
