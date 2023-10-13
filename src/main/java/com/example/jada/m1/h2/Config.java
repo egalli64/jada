64e1ffde42acf28a3b988712a7ddddb2824bfe5a
Emanuele <egalli64@gmail.com>
2023-10-13 20:44:58 +0200

    h2 setup

---
+++ src/main/java/com/example/jada/m1/h2/Config.java
@@ -0,0 +1,7 @@
+package com.example.jada.m1.h2;
+
+public final class Config {
+    public static final String URL = "jdbc:h2:./jdt";
+    public static final String USER = "sa";
+    public static final String PASSWORD = "";
+}
