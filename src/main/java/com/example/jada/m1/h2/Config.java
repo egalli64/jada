9e3bdb62de322d7bc023d2ced3bb7b697dc34b06
Emanuele <egalli64@gmail.com>
2023-10-15 21:44:18 +0200

    H2 review and test

--- src/main/java/com/example/jada/m1/h2/Config.java
+++ src/main/java/com/example/jada/m1/h2/Config.java
@@ -9,7 +9,7 @@ package com.example.jada.m1.h2;
  * JDBC for H2 basic configuration
  */
 public final class Config {
-    public static final String URL = "jdbc:h2:./jdt";
+    public static final String URL = "jdbc:h2:./jada";
     public static final String USER = "sa";
     public static final String PASSWORD = "";
 }
---
+++ src/test/java/com/example/jada/m1/h2/ConfigTest.java
@@ -0,0 +1,69 @@
+/*
+ * Introduction to Data Structures in Java
+ * 
+ * https://github.com/egalli64/jada
+ */
+package com.example.jada.m1.h2;
+
+import static com.example.jada.m1.h2.Config.PASSWORD;
+import static com.example.jada.m1.h2.Config.URL;
+import static com.example.jada.m1.h2.Config.USER;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class ConfigTest {
+    private static Connection connection;
+
+    @BeforeAll
+    static void init() {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            fail("Can't get connection", e);
+        }
+    }
+
+    @AfterAll
+    static void shutdown() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            fail("Can't close connection", e);
+        }
+    }
+
+    @Test
+    void product() {
+        try {
+            DatabaseMetaData dmd = connection.getMetaData();
+
+            String db = dmd.getDatabaseProductName();
+            assertThat(db).isEqualTo("H2");
+
+            String version = dmd.getDatabaseProductVersion();
+            assertThat(version).startsWith("2.2");
+        } catch (SQLException e) {
+            fail("Failure on metadata", e);
+        }
+    }
+
+    @Test
+    void catalog() {
+        try {
+            String catalog = connection.getCatalog();
+            assertThat(catalog).isEqualTo("JADA");
+        } catch (SQLException e) {
+            fail("Failure on metadata", e);
+        }
+    }
+}
