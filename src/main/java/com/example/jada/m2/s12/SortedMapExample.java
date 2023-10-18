3b519b4c2ab80d50bce45335022bd3f10628bebf
Emanuele <egalli64@gmail.com>
2023-10-18 21:33:47 +0200

    Sorted and NavigableMap

--- src/main/java/com/example/jada/m2/s12/SortedMapExample.java
+++ src/main/java/com/example/jada/m2/s12/SortedMapExample.java
@@ -6,51 +6,49 @@
 package com.example.jada.m2.s12;
 
 import java.util.Map;
+import java.util.NavigableMap;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 /**
- * The SortedMap interface
+ * The SortedMap and NavigableMap interfaces
  */
 public class SortedMapExample {
     public static void main(String[] args) {
         // A (modifiable) tree map
-        TreeMap<Integer, String> tree = new TreeMap<>(Map.ofEntries(Map.entry(2, "two")));
+        TreeMap<Integer, String> tree = new TreeMap<>(Map.ofEntries( //
+                Map.entry(1, "one"), Map.entry(2, "two"), Map.entry(3, "three")));
 
         // TreeMap is-a SortedMap
-        SortedMap<Integer, String> seq = tree;
-        System.out.println("A sequenced map: " + seq);
+        SortedMap<Integer, String> sorted = tree;
+        System.out.println("A sorted map: " + sorted);
 
-        /**
-         * Inserter
-         */
-        System.out.println("putFirst(1, \"one\") returns " + seq.putFirst(1, "one"));
-        System.out.println("putLast(3, \"three\") returns " + seq.putLast(3, "three"));
-        System.out.println("Now the sequenced map is " + seq);
+        try {
+            sorted.putFirst(1, "one");
+        } catch (UnsupportedOperationException ex) {
+            System.out.println("Can't putFirst() on a sorted map: " + ex);
+        }
 
-        /*
-         * Getters
-         */
-        System.out.println("firstEntry() returns " + seq.firstEntry());
-        System.out.println("lastEntry() returns " + seq.lastEntry());
+        try {
+            sorted.putLast(4, "four");
+        } catch (UnsupportedOperationException ex) {
+            System.out.println("Can't putLast() on a sorted map: " + ex);
+        }
 
-        /**
-         * Removers
-         */
-        System.out.println("pollFirstEntry() returns " + seq.pollFirstEntry());
-        System.out.println("pollLastEntry() returns " + seq.pollLastEntry());
-        System.out.println("Now the map is " + seq);
+        System.out.println("firstKey() is " + sorted.firstKey());
+        System.out.println("lastKey() is " + sorted.lastKey());
 
-        // making things a bit more interesting
-        seq.putFirst(1, "one");
+        System.out.println("subMap(2, 3) is " + sorted.subMap(2, 3));
+        System.out.println("headMap(2) is " + sorted.headMap(2));
+        System.out.println("tailMap(2) is " + sorted.tailMap(2));
 
-        /*
-         * Views
-         */
-        System.out.println("The sequencedKeySet() is " + seq.sequencedKeySet());
-        System.out.println("The sequencedValues() is " + seq.sequencedValues());
-        System.out.println("The sequencedEntrySet() is " + seq.sequencedEntrySet());
+        // TreeMap is-a NavigableMap
+        NavigableMap<Integer, String> nav = tree;
+        System.out.println("A navigable map: " + nav);
 
-        System.out.println("The reversed() sequence is " + seq.reversed());
+        System.out.println("lowerEntry(2) is " + nav.lowerEntry(2));
+        System.out.println("lowerKey(2) is " + nav.lowerKey(2));
+        System.out.println("floorEntry(2) is " + nav.floorEntry(2));
+        System.out.println("floorKey(2) is " + nav.floorKey(2));
     }
 }
