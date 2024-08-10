0410e0eb7b195cca1881c5254af20952675e6997
Emanuele <egalli64@gmail.com>
2024-08-10 16:09:55 +0200

    clarify

--- src/main/java/com/example/jada/m2/s09/MapExample.java
+++ src/main/java/com/example/jada/m2/s09/MapExample.java
@@ -14,24 +14,21 @@ import java.util.Map;
 public class MapExample {
     public static void main(String[] args) {
         // An unmodifiable map built from unmodifiable entries
-        Map<Integer, String> temp = Map.ofEntries( //
+        var temp = Map.ofEntries( //
                 Map.entry(1, "one"), //
                 Map.entry(2, "two"), //
                 Map.entry(3, "three"));
 
-        // A (modifiable) hash map
-        HashMap<Integer, String> hash = new HashMap<>(temp);
-
-        // HashMap is-a Map
-        Map<Integer, String> map = hash;
+        // Generate a modifiable HashMap from the unmodifiable one, then see it as a map
+        Map<Integer, String> map = new HashMap<>(temp);
         System.out.println("A map: " + map);
 
         /*
          * Getters
          */
         for (int key = 3; key < 5; key++) {
-            System.out.printf("get(%d) is %s%n", key, map.get(key));
-            System.out.printf("getOrDefault(%d) is %s%n", key, map.getOrDefault(key, "Missing"));
+            System.out.printf("get(%d) is %s\n", key, map.get(key));
+            System.out.printf("getOrDefault(%d) is %s\n", key, map.getOrDefault(key, "Missing"));
         }
 
         /**
@@ -39,8 +36,7 @@ public class MapExample {
          */
         for (int key = 3; key < 5; key++) {
             final String value = "four";
-            System.out.printf("putIfAbsent(%d, %s) returns %s%n", key, value, //
-                    map.putIfAbsent(key, value));
+            System.out.printf("putIfAbsent(%d, %s) returns %s\n", key, value, map.putIfAbsent(key, value));
         }
         System.out.println("Now the map is " + map);
 
@@ -49,16 +45,14 @@ public class MapExample {
          */
         for (int key = 4; key < 6; key++) {
             final String value = "vier";
-            System.out.printf("replace(%d, %s) returns %s%n", key, value, //
-                    map.replace(key, value));
+            System.out.printf("replace(%d, %s) returns %s\n", key, value, map.replace(key, value));
         }
         System.out.println("Now the map is " + map);
 
         for (int key = 3; key < 5; key++) {
             final String old = "vier";
             final String value = "four";
-            System.out.printf("replace(%d, %s, %b) returns %s%n", key, old, value, //
-                    map.replace(key, old, value));
+            System.out.printf("replace(%d, %s, %b) returns %s\n", key, old, value, map.replace(key, old, value));
         }
         System.out.println("Now the map is " + map);
     }
