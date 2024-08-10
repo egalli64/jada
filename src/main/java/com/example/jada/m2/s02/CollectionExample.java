7b21d7a0c5745acfb282293b82905c8fa94e1906
Emanuele <egalli64@gmail.com>
2024-08-10 09:57:30 +0200

    readability

--- src/main/java/com/example/jada/m2/s02/CollectionExample.java
+++ src/main/java/com/example/jada/m2/s02/CollectionExample.java
@@ -27,56 +27,65 @@ public class CollectionExample {
 
         if (!one.equals(two)) {
             System.out.println("Accordingly to equals(), collection one and two are different");
+        } else {
+            System.out.println("Unexpected! Collection of different types should be seen as different");
         }
 
         /*
          * size
          */
-        System.out.println("Collection one has size() " + one.size());
+        if (one.size() == two.size()) {
+            System.out.printf("Collection one and two have both %d as size()\n", one.size());
+        } else {
+            System.out.println("Unexpected!");
+        }
 
         /*
-         * contains, containsAll, isEmpty()
+         * contains, containsAll, isEmpty
          */
-        System.out.println("Collection one contains(42)? " + one.contains(42));
-        System.out.println("Collection one containsAll(two)? " + one.containsAll(two));
-        System.out.println("Collection one isEmpty()? " + one.isEmpty());
+        System.out.println("Collection one contains(42)? " + one.contains(42)); // true
+        System.out.println("Collection one containsAll(two)? " + one.containsAll(two)); // true
+        System.out.println("Collection one isEmpty()? " + one.isEmpty()); // false
 
         /*
          * add and addAll
          */
-        System.out.println("add(18) on collection two succeeded? " + two.add(18));
+        System.out.println("add(18) on collection two succeeded? " + two.add(18)); // true
 
         Collection<Integer> twoB = new HashSet<>();
         System.out.println("Collection twoB has been created empty: " + twoB);
-        System.out.println("Collection twoB has changed after addAll(two)? " + twoB.addAll(two));
+        System.out.println("Collection twoB has changed after addAll(two)? " + twoB.addAll(two)); // true
 
         if (two.equals(twoB)) {
-            System.out.println("Collection two equals() twoB (same concrete type and with the same elements)");
+            System.out.println("Collection two equals() twoB (same concrete type and same elements)");
+        } else {
+            System.out.println("Unexpected");
         }
 
         /*
          * remove and remove all
          */
-        System.out.println("remove(18) on collection twoB succeeded? " + twoB.remove(18));
-        System.out.println("Collection twoB contains(18)? " + twoB.contains(18));
-        System.out.println("Collection twoB has changed after removeAll(two)? " + twoB.removeAll(two));
-        System.out.println("Collection twoB isEmpty()? " + twoB.isEmpty());
+        System.out.println("remove(18) on collection twoB succeeded? " + twoB.remove(18)); // true
+        System.out.println("Collection twoB contains(18)? " + twoB.contains(18)); // false
+        System.out.println("Collection twoB has changed after removeAll(two)? " + twoB.removeAll(two)); // true
+        System.out.println("Collection twoB isEmpty()? " + twoB.isEmpty()); // true
 
         /*
          * clear
          */
         two.clear();
-        System.out.println("After clear(), collection two is empty? " + two.isEmpty());
+        System.out.println("After clear(), collection two is empty? " + two.isEmpty()); // true
 
         /*
          * toArray
          */
         System.out.print("Collection one as array: ");
 
-        Integer[] values = one.toArray(new Integer[one.size()]);
+        // among the three overload of toArray(), this is usually the preferred one
+        Integer[] values = one.toArray(new Integer[0]);
 
         // same, in two steps
-//        Integer[] values = new Integer[one.size()];
+//        Integer[] values = new Integer[0];
 //        values = one.toArray(values);
 
         System.out.println(Arrays.toString(values));
@@ -95,8 +104,9 @@ public class CollectionExample {
          */
         try {
             one.remove(42);
+            System.out.println("Unexpected! Collection one is unmodifiable");
         } catch (UnsupportedOperationException ex) {
-            System.out.println("No remove() on unmodifiable collection one: " + ex);
+            System.out.println("Can't remove() from an unmodifiable collection: " + ex);
         }
     }
 }
