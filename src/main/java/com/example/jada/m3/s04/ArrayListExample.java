f17a59c50d0c9678248abfa01564194c96607811
Emanuele <egalli64@gmail.com>
2024-08-11 14:20:39 +0200

    clarify

--- src/main/java/com/example/jada/m3/s04/ArrayListExample.java
+++ src/main/java/com/example/jada/m3/s04/ArrayListExample.java
@@ -18,25 +18,21 @@ public class ArrayListExample {
      * @param args not used
      */
     public static void main(String[] args) {
-        // Instantiate an empty list
-        List<Integer> list0 = new ArrayList<>();
-        System.out.println("An empty list: " + list0);
-
         // Instantiate an empty list with a given initial capacity
-        List<Integer> list1 = new ArrayList<Integer>(5);
-        System.out.println("An empty list w/ given (5) capacity: " + list1);
+        List<Integer> list = new ArrayList<Integer>(5);
+        System.out.println("An empty list w/ given (5) initial capacity: " + list);
 
         // adding an element in the last position
-        list1.add(56);
+        list.add(56);
         // adding an element in the first position
-        list1.add(0, 18);
+        list.add(0, 18);
         // adding an element in the first position (Java 21)
-        list1.addFirst(99);
+        list.addFirst(99);
 
-        list1.add(42);
-        System.out.println("After adding elements: " + list1);
+        list.add(42);
+        System.out.println("After adding elements: " + list);
 
-        ArrayList<Integer> list2 = new ArrayList<>(list1);
+        ArrayList<Integer> list2 = new ArrayList<>(list);
         System.out.println("A list by copy constructor: " + list2);
 
         System.out.println("First element is " + list2.get(0));
@@ -46,5 +42,6 @@ public class ArrayListExample {
         list2.remove(1);
 
         System.out.println("After changing and removing: " + list2);
+        System.out.println("The original list has not changed: " + list);
     }
 }
